e39447917254f9cd6f88f41edb8a56e437f20e7d
庄永新 <zhuang_yongxin@163.com>
2015-05-05 09:30:03 +0800

    Create Input.h

---
+++ Input.h
@@ -0,0 +1,24 @@
+#if !defined (INPUT_H)
+#define INPUT_H
+
+const int maxBuf = 100;
+
+// Tokens are tokNumber, tokError, +, -, *, /.
+
+const int tokNumber = 1;
+const int tokError  = 2;
+
+// Gets input from stdin, converts to token.
+
+class Input
+{
+public:
+	Input ();
+	int Token () const { return _token; }
+	int Number () const;
+private:
+	int  _token;
+	char _buf [maxBuf];
+};
+
+#endif
